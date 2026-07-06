419a864f9d1b47e459fa30582eb60f1cda2d5187
Byanajjara Ganesha <byanajjaraganesha@gmail.com>
2026-07-06 08:45:13 +0530

    Update Imm_Adder.v

--- 01_RTL_Blocks/Immediate_Adder_Unit/Imm_Adder.v
+++ 01_RTL_Blocks/Immediate_Adder_Unit/Imm_Adder.v
@@ -1,1 +1,22 @@
 
+//code for Imm_Adder
+
+module Imm_Adder(
+    input ladder_src_in,
+    input [31:0] PC_in,
+    input [31:0] Src_Data1_in,
+    input [31:0] imm_Data_in,
+    output reg [31:0] Added_data_out
+);
+
+always @(*) begin
+
+    if (ladder_src_in)
+        Added_data_out = PC_in + imm_Data_in;
+
+    else
+        Added_data_out = Src_Data1_in + imm_Data_in;
+
+end
+       
+ endmodule
